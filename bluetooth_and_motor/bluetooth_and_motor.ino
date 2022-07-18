b5b22994a0a36feb2da20a8e4e48b721b13cbd28
Kevin <kevin12kim27@gmail.com>
2022-07-17 18:03:28 -0700

    added sensor logic

--- bluetooth_and_motor/bluetooth_and_motor.ino
+++ bluetooth_and_motor/bluetooth_and_motor.ino
@@ -1,37 +1,37 @@
 #include <SoftwareSerial.h>
 
 //channel pins for encoder values
-int channelA = 5;
-int channelB = 4;
+int channelA = 10;
+int channelB = 9;
 int pwmPin = 6;
 
 //output pins to control motor input
-int in1Pin = 10;
-int in2Pin = 9;
+int in1Pin = 5;
+int in2Pin = 4;
 
 //encoder channel values
-int valA= 0;
-int valB= 0;
+int valA = 0;
+int valB = 0;
 
 //for counting purposes
-int valAPrev=valA;
+int valAPrev = valA;
 
 //motor pwm duty cycle
 int motorVal = 200;
 
 //touch sensor pin NOTE CHANGE THIS
-int touchPin = 7;
+int touchPin = 1;
 
-int motorVals[] = {125,150,175, 200, 225,250};
+int motorVals[] = {125, 150, 175, 200, 225, 250};
 int motorIndex = 0;
-int bufIndex=0;
+int bufIndex = 0;
 int buf[30];
 
 //motor direction
 int dir = 0;
 int brightness = 0;
 //
-volatile int pulses =0 ;
+volatile int pulses = 0 ;
 
 //timing
 long previousMillis = 0;
@@ -52,14 +52,14 @@ int prevstate = 0;
 int state = 0;
 SoftwareSerial bluetoothSerial(rxPin, txPin); // RX, TX
 
-void onTouch(){
-    analogWrite(pwmPin, 0);
-    Serial.print("HELLO");
+void onTouch() {
+  analogWrite(pwmPin, 100);
+  Serial.print("HELLO");
 
 }
 void setup() {
   //BLUETOOTH SETUP
-  Serial.begin(9600);
+  //Serial.begin(9600);
   bluetoothSerial.begin(9600);
 
   //MOTOR SETUP
@@ -67,23 +67,32 @@ void setup() {
   pinMode(pwmPin, OUTPUT);
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
-  pinMode(touchPin, INPUT);
+  pinMode(touchPin, INPUT_PULLUP);
+
 
   //attachInterrupt(digitalPinToInterrupt(channelB), count_event, RISING);
-  attachInterrupt(digitalPinToInterrupt(touchPin), onTouch, RISING);
+  //attachInterrupt(digitalPinToInterrupt(touchPin), onTouch, FALLING);
 
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-
-  if (digitalRead(touchPin) == HIGH){
-    Serial.print("HIGH");
+  if (digitalRead(touchPin) == 0)
+  {
+    analogWrite(pwmPin, 255);
+    //Serial.print("TOUCH");
+  }
+  else {
+    analogWrite(pwmPin, 0);
   }
 
-  digitalWrite(in1Pin, dir);
-  digitalWrite(in2Pin, !dir);
+  /*if (digitalRead(touchPin) == HIGH){
+    Serial.print("HIGH");
+    }*/
+
+
+
   while (bluetoothSerial.available()) {
     state = bluetoothSerial.read();
     //Serial.println(state);
@@ -96,6 +105,20 @@ void loop() {
 
 
   }
+
+  digitalWrite(in1Pin, dir);
+  digitalWrite(in2Pin, !dir);
+
+  /*state = bluetoothSerial.read();
+    if (state != prevstate)
+    {
+    //Serial.println(state);
+    prevstate = state;
+    handleState();
+    }*/
+
+
+
   //Serial.println("State: " + state);
 
   // put your main code here, to run repeatedly:
@@ -105,7 +128,7 @@ void loop() {
 void handleState() {
   switch (state) {
     case 39:
-      dir = 0; 
+      dir = 0;
       Serial.println("UP TRUE");
       analogWrite(pwmPin, motorVal);
 
@@ -117,7 +140,7 @@ void handleState() {
       break;
     case 37:
       Serial.println("DOWN TRUE");
-      dir =1;
+      dir = 1;
       analogWrite(pwmPin, motorVal);
 
       break;
@@ -126,8 +149,11 @@ void handleState() {
       analogWrite(pwmPin, 0);
 
       break;
+    default:
+      analogWrite(pwmPin, 0);
+      break;
   }
 }
-void count_event(){
+void count_event() {
   pulses++;
 }
